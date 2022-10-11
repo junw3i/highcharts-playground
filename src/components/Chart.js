17d04d1c2f5f113b86c70f3774942fb8adfe1e2a
junw3i <chanjunwei@gmail.com>
2022-10-11 12:45:29 +0800

    initial

---
+++ src/components/Chart.js
@@ -0,0 +1,26 @@
+// React component
+
+import React from 'react'
+import Highcharts from 'highcharts'
+import HighchartsReact from 'highcharts-react-official'
+
+const options = {
+  title: {
+    text: 'My chart',
+  },
+  series: [
+    {
+      data: [1, 2, 3],
+    },
+  ],
+}
+
+const Chart = () => {
+  return (
+    <div>
+      <HighchartsReact highcharts={Highcharts} options={options} />
+    </div>
+  )
+}
+
+export default Chart

42b43aa2fc8135abfbd275c703c72a7b35946558
junw3i <chanjunwei@gmail.com>
2022-10-12 10:51:29 +0800

    fetch eth priciing

--- src/components/Chart.js
+++ src/components/Chart.js
@@ -1,10 +1,8 @@
-// React component
-
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Highcharts from 'highcharts'
 import HighchartsReact from 'highcharts-react-official'
 
-const options = {
+let options = {
   title: {
     text: 'My chart',
   },
@@ -15,7 +13,31 @@ const options = {
   ],
 }
 
+function scrub(d) {
+  // [0] unix
+  // [1] close
+  const scrubbed = []
+  for (var i = d.length - 1; i >= 0; i--) {
+    scrubbed.push([d[i][0], d[i][4]])
+  }
+  return scrubbed
+}
+
 const Chart = () => {
+  const [data, setData] = useState([])
+
+  useEffect(() => {
+    fetch('https://api.exchange.coinbase.com/products/ETH-USD/candles?granularity=86400')
+      .then(response => response.json())
+      .then(d => {
+        const srubbedData = scrub(d)
+        setData(srubbedData)
+      })
+  }, [])
+
+  console.log(data)
+  options.series[0].data = data
+
   return (
     <div>
       <HighchartsReact highcharts={Highcharts} options={options} />
